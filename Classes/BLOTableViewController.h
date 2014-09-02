5a2d2d58b5963c61f5aee5ed7775d68d269da624
Miroslaw Kucharzyk <miroslaw.kucharzyk@schibsted.pl>
2014-09-02 14:58:13 +0200

    initial configuration

---
+++ Classes/BLOTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLOTableViewController.h
+//  Photo-viewer
+//
+//  Created by Mirosław Kucharzyk on 02/09/14.
+//  Copyright (c) 2014 STP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLOTableViewController : UITableViewController
+
+@end
